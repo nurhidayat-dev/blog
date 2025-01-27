f011610c4f29ef20d7f4e68f270110c58b44f942
Taufik Nurhidayat <fiik346@gmail.com>
2025-01-27 16:50:58 +0700

    idk

--- eleventy.config.mjs
+++ eleventy.config.mjs
@@ -1,30 +1,124 @@
 // Any combination of these
-import { I18nPlugin, RenderPlugin, HtmlBasePlugin } from "@11ty/eleventy";
-import postcss from "postcss";
-import tailwind from "@tailwindcss/postcss";
-import plugins from "./src/_configs/plugins/index.js";
+import {
+  IdAttributePlugin,
+  InputPathToUrlTransformPlugin,
+  HtmlBasePlugin,
+} from "@11ty/eleventy";
+import { feedPlugin } from "@11ty/eleventy-plugin-rss";
+import pluginSyntaxHighlight from "@11ty/eleventy-plugin-syntaxhighlight";
+import pluginNavigation from "@11ty/eleventy-navigation";
 
-export default function (eleventyConfig) {
+import pluginFilters from "./_config/filters.js";
+
+export default async function (eleventyConfig) {
   eleventyConfig.addWatchTarget("./src/styles/**/*.css");
 
-  ["src/assets/fonts/"].forEach((path) =>
-    eleventyConfig.addPassthroughCopy(path),
-  );
+  eleventyConfig.addPreprocessor("drafts", "*", (data, content) => {
+    if (data.draft && process.env.ELEVENTY_RUN_MODE === "build") {
+      return false;
+    }
+  });
 
-  // plugins
-  eleventyConfig.addPlugin(plugins.drafts);
+  eleventyConfig
+    .addPassthroughCopy({
+      "./public/": "/",
+    })
+    .addPassthroughCopy("./content/feed/pretty-atom-feed.xsl");
 
-  eleventyConfig.setLibrary("md", plugins.markdownLib);
+  eleventyConfig.addWatchTarget("content/**/*.{svg,webp,png,jpg,jpeg,gif}");
 
   // bundle
-  eleventyConfig.addBundle("css", { hoist: true });
+  eleventyConfig.addBundle("css", {
+    toFileDirectory: "dist",
+  });
+  // Adds the {% js %} paired shortcode
+  eleventyConfig.addBundle("js", {
+    toFileDirectory: "dist",
+  });
+  eleventyConfig.addPlugin(pluginSyntaxHighlight, {
+    preAttributes: { tabindex: 0 },
+  });
+  eleventyConfig.addPlugin(pluginNavigation);
+  eleventyConfig.addPlugin(HtmlBasePlugin);
+  eleventyConfig.addPlugin(InputPathToUrlTransformPlugin);
 
-  return {
-    dir: {
-      outuput: "_site",
-      input: "src",
-      includes: "_includes",
-      layouts: "_layouts",
+  eleventyConfig.addPlugin(feedPlugin, {
+    type: "atom", // or "rss", "json"
+    outputPath: "/feed/feed.xml",
+    stylesheet: "pretty-atom-feed.xsl",
+    templateData: {
+      eleventyNavigation: {
+        key: "Feed",
+        order: 4,
+      },
     },
-  };
+    collection: {
+      name: "posts",
+      limit: 10,
+    },
+    metadata: {
+      language: "en",
+      title: "Blog Title",
+      subtitle: "This is a longer description about your blog.",
+      base: "https://example.com/",
+      author: {
+        name: "Your Name",
+      },
+    },
+  });
+
+  // Image optimization: https://www.11ty.dev/docs/plugins/image/#eleventy-transform
+  eleventyConfig.addPlugin(eleventyImageTransformPlugin, {
+    // Output formats for each image.
+    formats: ["avif", "webp", "auto"],
+
+    // widths: ["auto"],
+
+    failOnError: false,
+    htmlOptions: {
+      imgAttributes: {
+        // e.g. <img loading decoding> assigned on the HTML tag will override these values.
+        loading: "lazy",
+        decoding: "async",
+      },
+    },
+
+    sharpOptions: {
+      animated: true,
+    },
+  });
+
+  // Filters
+  eleventyConfig.addPlugin(pluginFilters);
+
+  eleventyConfig.addPlugin(IdAttributePlugin, {
+    // by default we use Eleventy’s built-in `slugify` filter:
+    // slugify: eleventyConfig.getFilter("slugify"),
+    // selector: "h1,h2,h3,h4,h5,h6", // default
+  });
+
+  eleventyConfig.addShortcode("currentBuildDate", () => {
+    return new Date().toISOString();
+  });
 }
+
+export const config = {
+  // Control which files Eleventy will process
+  // e.g.: *.md, *.njk, *.html, *.liquid
+  templateFormats: ["md", "njk", "html", "liquid", "11ty.js"],
+  // Control which files Eleventy will process
+  // e.g.: *.md, *.njk, *.html, *.liquid
+  templateFormats: ["md", "njk", "html", "liquid", "11ty.js"],
+
+  // Pre-process *.md files with: (default: `liquid`)
+  markdownTemplateEngine: "njk",
+
+  // Pre-process *.html files with: (default: `liquid`)
+  htmlTemplateEngine: "njk",
+  dir: {
+    outuput: "_site",
+    input: "src",
+    includes: "_includes",
+    layouts: "_layouts",
+  },
+};
